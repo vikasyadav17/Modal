9757b50f078509f18248fbd7c4a863ae81cc110c
vikas yadav <yadvikas610@gmail.com>
2021-03-03 20:17:35 +0530

    "Modal using vanilla java(good example to understand querySelectorAll)"

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+const show = document.querySelectorAll('.show-modal');
+const close = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+for (let i = 0; i < show.length; i++) {
+  show[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+close.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
